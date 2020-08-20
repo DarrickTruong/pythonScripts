766280c58919c30df9e497f41931f0e6c32dc467
DarrickTruong <55263805+DarrickTruong@users.noreply.github.com>
2020-08-19 19:02:38 -0700

    script

--- gie.py
+++ gie.py
@@ -3,15 +3,16 @@ import re
 import os
 
 os.chdir('/Users/DarrickT/Downloads/')
-wb = openpyxl.load_workbook('sales.xlsx')
-sheet = wb.get_sheet_by_name('Page 1')
+wb = openpyxl.load_workbook('surfraw.xlsx')
+sheet = wb['Page 1']
 def oldestDate():
-    for i in range(2, 9474):migration
+    for i in range(2, 8657):
         print(sheet.cell(row=i,column=26).value)
         all1 = re.findall(r"[\d]{4}-[\d]{2}-[\d]{2}", sheet.cell(row=i,column=26).value)
         all2 = re.findall(r"[\d]{4}-[\d]{2}-[\d]{2}", sheet.cell(row=i,column=27).value)
         if len(all1) !=0:
-            sheet.cell(row=i, column=51).value = all1[len(all1)-1]
+            sheet.cell(row=i, column=52).value = all1[len(all1)-1]
         if len(all2) !=0:
-            sheet.cell(row=i, column=52).value = all2[len(all2)-1]
-    wb.save('sales1.xlsx')
+            sheet.cell(row=i, column=53).value = all2[len(all2)-1]
+    wb.save('surfraw1.xlsx')
+
